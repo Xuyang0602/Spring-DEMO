2be61491a4226073c8debb032df8ee6733811318
xuyangwang0827 <xuyangwang0827@gmail.com>
2019-10-29 16:01:07 -0700

    Injecting Literal Values

--- src/Demo/BasketballCoach.java
+++ src/Demo/BasketballCoach.java
@@ -1,6 +1,29 @@
 package Demo;
 
 public class BasketballCoach implements Coach{
+
+    // Add new fields for email address and team
+    private String emailAddress;
+    private String team;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("Basketball Coach: inside setter email Address");
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("Basketball Coach: inside setter team");
+        this.team = team;
+    }
+
     private FortuneService fortuneService;
     // Create a no-arg constructor
     public BasketballCoach() {
